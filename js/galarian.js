46144a7396cb681b9c6f9780eeb771ed292fa2f6
Sneha-005 <160414790+Sneha-005@users.noreply.github.com>
2024-09-14 20:16:58 +0530

    Create galarian.js

---
+++ js/galarian.js
@@ -0,0 +1,9 @@
+function runCheck(){
+  var data= document.getElementById("name").value;
+  if(data=="galarian rapidash"){
+      location.replace("galarian rapidash.html");
+  }
+  else{
+      location.replace("loss.html");
+  }
+}
