2f8dea651fc57dcf6a1037e701b8ce747826ad8b
Sneha-005 <160414790+Sneha-005@users.noreply.github.com>
2024-09-14 20:15:49 +0530

    Create crobat.js

---
+++ js/crobat.js
@@ -0,0 +1,9 @@
+function runCheck(){
+  var data= document.getElementById("name").value;
+  if(data=="crobat"){
+      location.replace("crobat.html");
+  }
+  else{
+      location.replace("loss.html");
+  }
+}
