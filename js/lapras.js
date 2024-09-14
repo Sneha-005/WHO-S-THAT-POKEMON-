3d050e3d25349da58d2323367e78d854c6d826dc
Sneha-005 <160414790+Sneha-005@users.noreply.github.com>
2024-09-14 20:16:14 +0530

    Create lapras.js

---
+++ js/lapras.js
@@ -0,0 +1,9 @@
+function runCheck(){
+  var data= document.getElementById("name").value;
+  if(data=="lapras"){
+      location.replace("lapras.html");
+  }
+  else{
+      location.replace("loss.html");
+  }
+}
